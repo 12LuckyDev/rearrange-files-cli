e862f2a93b46709daa582d5caac8ed447c71e9bf
12LuckyDev <12luckydev@gmail.com>
2025-01-28 14:42:33 +0100

    Add basic arguments

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("HELLO ( ͡° ͜ʖ ͡°)");
+import { RearrangeOptionsModel } from "rearrange-files";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+	.arguments("[srcPath] [targetPath]")
+	.action((srcPath?: string, targetPath?: string) => {
+		const opt: RearrangeOptionsModel = { srcPath: srcPath ?? process.cwd() };
+		if (targetPath) {
+			opt.targetPath = targetPath;
+		}
+		console.log(opt);
+	})
+	.description(
+		"A cli that allows you to reorganize the structure of files based on their creation date."
+	);
+
+program.parse(process.argv);
